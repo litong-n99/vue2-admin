51891ac33f5de4a9e431e6dee1374aa30ab175a4
litong <tjlitong@travelsky.com>
2022-04-20 20:27:14 +0800

    feat: update ci/cd

--- script/delUnUsedCache.js
+++ script/delUnUsedCache.js
@@ -5,11 +5,11 @@ const cachePath = path.join(__dirname, '../node_modules/.cache/turbo');
 
 if (fs.existsSync(cachePath)) {
   const dirs = fs.readdirSync(cachePath);
-  const weekMs = 1000 * 60 * 60 * 24 * 7;
+  const timeoutMs = 1000 * 60 * 60 * 24 * 30;
   const currentMs = Date.now();
   dirs.forEach(item => {
     const filePath = path.join(__dirname, '../node_modules/.cache/turbo', item);
-    if ((currentMs - fs.statSync(filePath).mtimeMs) > weekMs) {
+    if ((currentMs - fs.statSync(filePath).mtimeMs) > timeoutMs) {
       console.log('remove cache file', filePath);
       fs.removeSync(filePath);
     }
